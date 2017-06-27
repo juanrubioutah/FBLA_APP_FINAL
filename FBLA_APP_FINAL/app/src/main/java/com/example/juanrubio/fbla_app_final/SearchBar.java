b107c4ddb95972023dff2f66605d147bc11e3e96
YaFeng-Xiong <yafeng.xiong110@gmail.com>
2017-06-26 18:35:44 -0600

    new activity: search bar
    hope no bugs

---
+++ FBLA_APP_FINAL/app/src/main/java/com/example/juanrubio/fbla_app_final/SearchBar.java
@@ -0,0 +1,13 @@
+package com.example.juanrubio.fbla_app_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_bar);
+    }
+}
