ae86d17ed17c78da5b8eb1f9af5c80f618465f5a
YaFeng-Xiong <yafeng.xiong110@gmail.com>
2017-06-28 13:25:40 -0600

    meow?

--- FBLA_APP_FINAL/app/src/main/java/com/example/juanrubio/fbla_app_final/ListView.java
+++ FBLA_APP_FINAL/app/src/main/java/com/example/juanrubio/fbla_app_final/ListView.java
@@ -1,5 +1,6 @@
 package com.example.juanrubio.fbla_app_final;
 
+import android.content.res.Resources;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -11,29 +12,38 @@ import java.util.ArrayList;
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public class ListView extends AppCompatActivity {
 
-    ItemManager itemManager = MainActivity.getGlobalItemManager();
-    CartManager cartManager = MainActivity.getGlobalCartManager();
+    //ItemManager itemManager = MainActivity.getGlobalItemManager();
+    //CartManager cartManager = MainActivity.getGlobalCartManager();
+    ListView list;
+    String[] titles;
+    String[] prices;
+    int[] imgs = {R.drawable.yellow_chair, R.drawable.purse, R.drawable.keyboard, R.drawable.coffee_maker,
+            R.drawable.textbook, R.drawable.coat};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
-        ///ListView listView = (ListView)findViewById(R.id.list_view);
+        Resources res = getResources();
+
+        titles = res.getStringArray(R.array.itemArray);
+        prices = res.getStringArray(R.array.priceArray);
+
+        //list = (ListView)findViewById(R.id.list_view);
 
         android.widget.ListView listView = (android.widget.ListView)findViewById(R.id.list_view);
 
         ArrayList<Product> products = new ArrayList<Product>();
 
 
-        //somehow get all the array info here
-
 
         CustomAdapter adapter = new CustomAdapter(getApplicationContext(),R.layout.single_row, products);
         //ListView.setAdapter(adapter);
 
-        String[] itemNames = itemManager.getAllItemNames();
-        String[] cartNames = cartManager.getItemNames();
+        //String[] itemNames = itemManager.getAllItemNames();
+        //String[] cartNames = cartManager.getItemNames();
+        
         //CustomAdapter adapter = new CustomAdapter(getApplicationContext(),R.layout.single_row, );
     }
 }
