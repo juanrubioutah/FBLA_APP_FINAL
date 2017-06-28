e120caa0e05f42728e48d70f4e238bf84ec38f31
Juan Rubio <juanrubioutah@gmail.com>
2017-06-28 09:09:35 +0200

    Quick Fixes
    Fixed some of the more major errors and also got you the string arrays
    of names so you can use them as you please.

--- FBLA_APP_FINAL/app/src/main/java/com/example/juanrubio/fbla_app_final/ListView.java
+++ FBLA_APP_FINAL/app/src/main/java/com/example/juanrubio/fbla_app_final/ListView.java
@@ -1,24 +1,30 @@
 package com.example.juanrubio.fbla_app_final;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
 
 import java.util.ArrayList;
 
-
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public class ListView extends AppCompatActivity {
+
+    ItemManager itemManager = MainActivity.getGlobalItemManager();
+    CartManager cartManager = MainActivity.getGlobalCartManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-        ListView listView = (ListView)findViewById(R.id.list_view);
+        android.widget.ListView listView = (android.widget.ListView)findViewById(R.id.list_view);
         ArrayList<Product> products = new ArrayList<Product>();
 
 
         //somehow get all the array info here
-
-        CustomAdapter adapter = new CustomAdapter(getApplicationContext(),R.layout.single_row, )
+        String[] itemNames = itemManager.getAllItemNames();
+        String[] cartNames = cartManager.getItemNames();
+        CustomAdapter adapter = new CustomAdapter(getApplicationContext(),R.layout.single_row, );
     }
 }
